43c6cc3262b2e525eb861e60c44e3622a98a79f9
Vinamauriza31 <e100221173@student.ums.ac.id>
2025-01-14 20:44:45 +0700

    Add files via upload

---
+++ styles/Potensi_Resapan_Air_1_style.js
@@ -0,0 +1,72 @@
+var size = 0;
+var placement = 'point';
+function categories_Potensi_Resapan_Air_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'Baik':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(26,150,65,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Agak Baik':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(166,217,106,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Agak Buruk':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(251,251,57,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Buruk':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(235,147,62,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Sangat Buruk':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(215,25,28,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_Potensi_Resapan_Air_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("Ket");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_Potensi_Resapan_Air_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
